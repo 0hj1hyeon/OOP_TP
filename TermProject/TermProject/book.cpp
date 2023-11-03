753dd86cd2022d5747b0ac658dc92a09ada97234
0hj1hyeon <ojh010911@gmail.com>
2023-11-03 23:30:24 +0900

    class 만들기

--- TermProject/TermProject/book.cpp
+++ TermProject/TermProject/book.cpp
@@ -1,1 +1,79 @@
 #include "book.h"
+
+// 생성자 구현
+Book::Book(const std::string& bookName, const std::string& writer, const std::string& publisher, const std::string& genre, float numbering, float starPoint, const std::string& review, int page, bool rent)
+    : bookname(bookName), writer(writer), publisher(publisher), genre(genre), numbering(numbering), star_point(starPoint), review(review), page(page), rent(rent) {}
+
+// 접근자 메서드 구현
+std::string Book::getBookName() const {
+    return bookname;
+}
+
+std::string Book::getWriter() const {
+    return writer;
+}
+
+std::string Book::getPublisher() const {
+    return publisher;
+}
+
+std::string Book::getGenre() const {
+    return genre;
+}
+
+float Book::getNumbering() const {
+    return numbering;
+}
+
+float Book::getStarPoint() const {
+    return star_point;
+}
+
+std::string Book::getReview() const {
+    return review;
+}
+
+int Book::getPage() const {
+    return page;
+}
+
+bool Book::isRented() const {
+    return rent;
+}
+
+// 수정자 메서드 구현
+void Book::setBookName(const std::string& bookName) {
+    bookname = bookName;
+}
+
+void Book::setWriter(const std::string& writer) {
+    this->writer = writer;
+}
+
+void Book::setPublisher(const std::string& publisher) {
+    this->publisher = publisher;
+}
+
+void Book::setGenre(const std::string& genre) {
+    this->genre = genre;
+}
+
+void Book::setNumbering(float numbering) {
+    this->numbering = numbering;
+}
+
+void Book::setStarPoint(float starPoint) {
+    star_point = starPoint;
+}
+
+void Book::setReview(const std::string& review) {
+    this->review = review;
+}
+
+void Book::setPage(int page) {
+    this->page = page;
+}
+
+void Book::setRentStatus(bool rent) {
+    this->rent = rent;
+}
--- TermProject/TermProject/book.h
+++ TermProject/TermProject/book.h
@@ -1,3 +1,45 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class Book {
+private:
+    std::string bookname; // 책이름
+    std::string writer;   // 저자
+    std::string publisher; // 출판사
+    std::string genre;    // 책 장르(분야)
+    float numbering;      // 책 고유번호
+    float star_point;     // 별점
+    std::string review;   // 리뷰
+    int page;             // 페이지 수
+    bool rent;            // 대여 여부
+
+
+public:
+    // 생성자
+    Book(const std::string& bookName, const std::string& writer, const std::string& publisher, const std::string& genre, float numbering, float starPoint, const std::string& review, int page, bool rent);
+
+    // 접근자 (Getter)
+    std::string getBookName() const;
+    std::string getWriter() const;
+    std::string getPublisher() const;
+    std::string getGenre() const;
+    float getNumbering() const;
+    float getStarPoint() const;
+    std::string getReview() const;
+    int getPage() const;
+    bool isRented() const;
+
+    // 수정자 (Setter)
+    void setBookName(const std::string& bookName);
+    void setWriter(const std::string& writer);
+    void setPublisher(const std::string& publisher);
+    void setGenre(const std::string& genre);
+    void setNumbering(float numbering);
+    void setStarPoint(float starPoint);
+    void setReview(const std::string& review);
+    void setPage(int page);
+    void setRentStatus(bool rent);
+
+
+};
